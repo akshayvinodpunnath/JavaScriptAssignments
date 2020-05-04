e821da35c05d08f6edcf27148ae26c422855a0f6
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-04 23:02:39 +0530

    git assignment 1 Completed

---
+++ assignment.js
@@ -0,0 +1,14 @@
+const userInput = 10 ;
+
+let result ;
+
+result = 18 + userInput;
+
+result = result - 10;
+result = result * 3;
+result = result / 3;
+
+alert(`result value - ${result}`)
+alert(`User Input value - ${userInput}`)
+
+
