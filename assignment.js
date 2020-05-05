512b2c3020209dbec4254815c0f88b1715822689
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 21:43:27 +0530

    Assignment 2 - Starting

--- assignment.js
+++ assignment.js
@@ -1,14 +1,1 @@
-const userInput = 10 ;
-
-let result ;
-
-result = 18 + userInput;
-
-result = result - 10;
-result = result * 3;
-result = result / 3;
-
-alert(`result value - ${result}`)
-alert(`User Input value - ${userInput}`)
-
-
+const task3Element = document.getElementById('task-3');
