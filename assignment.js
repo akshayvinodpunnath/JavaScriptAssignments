17995a6f74a3874d3050639dbfdca711e26f6958
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 21:46:39 +0530

    Assignment 2 - step1

--- assignment.js
+++ assignment.js
@@ -1,1 +1,9 @@
 const task3Element = document.getElementById('task-3');
+
+function showAlert() {
+    alert('Inside showAlert function');
+}
+
+function showName(name) {
+    alert(`Name is: ${name}`);
+}
