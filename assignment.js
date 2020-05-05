534277d3075b7692176259aaaae1943a5dea74a1
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-05 21:51:00 +0530

    Assignment 2 (functions) - step4 - threestring function

--- assignment.js
+++ assignment.js
@@ -1,5 +1,6 @@
 const task3Element = document.getElementById('task-3');
 
+//Function Declared
 function showAlert() {
     alert('Inside showAlert function');
 }
@@ -8,7 +9,14 @@ function showName(name) {
     alert(`Name is: ${name}`);
 }
 
+//calling both showAlert and showName
 //showAlert();
 //showName('Rocky');
 
+//Adding eventList to task3Element
 task3Element.addEventListener('click', showAlert);
+
+//new function with 3 string arguments
+function threeStrings(string1, string2, string3) {
+    return `${string1} ${string2} ${string3}`
+}
