0e056b4ec1bcaded4b875122b98bea74070c9f07
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-09 22:57:44 +0530

    assignment3 - step2

--- assignment.js
+++ assignment.js
@@ -3,3 +3,13 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 if (randomNumber > 0.7) {
     alert('Some Message')
 }
+
+const arrayNumbers = [1,2,3,4,5];
+
+for ( let i=0 ; i < arrayNumbers.length; i++) {
+    console.log(arrayNumbers[i])
+}
+
+for (let an of arrayNumbers) {
+    console.log(an)
+}
