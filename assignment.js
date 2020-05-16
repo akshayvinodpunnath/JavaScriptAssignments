696aff5a38e0f1f86ee99333a6f2847d4a59da0f
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-16 18:46:37 +0530

    initial commit assignment4

--- assignment.js
+++ assignment.js
@@ -1,24 +1,5 @@
-const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
-
-if (randomNumber > 0.7) {
-    alert('Some Message')
+function sayHello(name) {
+  console.log('Hi ' + name);
 }
 
-const arrayNumbers = [1,2,3,4,5];
-
-for ( let i=arrayNumbers.length-1 ; i>=0; i--) {
-    console.log(arrayNumbers[i])
-}
-
-for (let an of arrayNumbers) {
-    console.log(an)
-}
-
-const newRandomNumber = Math.random();
-
-if ( (newRandomNumber > 0.7 && randomNumber >0.7) || (newRandomNumber < 0.2 || randomNumber < 0.2)) {
-    alert('condition success')
-}
-
-console.log(randomNumber);
-console.log(newRandomNumber);
+sayHello();
