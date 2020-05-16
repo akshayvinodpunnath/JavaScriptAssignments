eb8807fb8a3a58b218de590184cfe56aebdf18a9
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-16 18:52:41 +0530

    assignment4 - step2, with 2 arguments

--- assignment.js
+++ assignment.js
@@ -1,9 +1,14 @@
-const sayHello = (name) => {
-  console.log('Hi ' + name);
-}
+// const sayHello = (name) => {
+//   console.log('Hi ' + name);
+// }
 
 // function sayHello(name) {
 //   console.log('Hi ' + name);
 // }
 
-sayHello('Akshay');
+//with 2 arguments
+const sayHello = (hello, name) => {
+  console.log(`${hello} ${name}`);
+}
+
+sayHello('Hi','Akshay');
