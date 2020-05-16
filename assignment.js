27a7ab8c309814ccfbbc19dde124b3708c4afaf8
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-17 00:54:23 +0530

    Assignment5 - step1

---
+++ assignment.js
@@ -0,0 +1,7 @@
+const task1Selection1 = document.getElementById('task-1');
+
+task1Selection1.style.backgroundColor = 'black';
+
+const task1Selection2 = document.querySelector('ol li')
+
+task1Selection2.style.color = 'white'
