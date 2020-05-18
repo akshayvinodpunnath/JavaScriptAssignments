d8357e23fe50a1a20a5488f315daaf9ca3d9f7aa
Akshay Vinod Punnath <akshayvinodpunnath@gmail.com>
2020-05-18 21:45:03 +0530

    Assignment5 - step2

--- assignment.js
+++ assignment.js
@@ -5,3 +5,11 @@ task1Selection1.style.backgroundColor = 'black';
 const task1Selection2 = document.querySelector('ol li')
 
 task1Selection2.style.color = 'white'
+
+const titleChange = document.head.getElementsByTagName('title');
+
+titleChange[0].innerText = 'Assignment - Solved!'
+
+const titleChange2 = document.querySelector('title')
+
+titleChange2.textContent = 'Assignment - Solved - 2!'
